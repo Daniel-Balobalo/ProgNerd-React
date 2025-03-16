f23d4a06fa7c30c480b9490929878355cbe8c5f0
Daniel Balobalo <70095157+Daniel-Balobalo@users.noreply.github.com>
2025-03-17 07:58:04 +0800

    3/17/2025 Commit

--- first-react-project/src/Footer.jsx
+++ first-react-project/src/Footer.jsx
@@ -1,4 +1,10 @@
 function Footer(){
+    return (
+            <footer>
+                <p>© 2021 React App</p>
+                <p>Created by: <a href="https://www.linkedin.com/in/your-linkedin-profile">Your Name</a></p>
+            </footer>
+            );
 
 }
 
